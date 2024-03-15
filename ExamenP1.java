ff22376573826e49d9d0ea32bb47a74dbf156a4f
samuel6025736 <samuel@nxtagro.io>
2024-03-15 00:02:51 -0600

    Add random coords to nodes and the mirrors

--- ExamenP1.java
+++ ExamenP1.java
@@ -1,11 +1,12 @@
 //Samuel Gutierrez Barona 22310388 4P
 
+import java.util.Random;
 import java.util.Vector;
 
 public class ExamenP1 {
     public static void main(String[] args) {
-        int filas = 4;
-        int columnas = 4;
+        int filas = 10;
+        int columnas = 10;
 
         Vector<Vector<Integer>> matriz = new Vector<>(filas);
 
@@ -15,7 +16,19 @@ public class ExamenP1 {
                 fila.add(0);
             }
             matriz.add(fila);
+        }
 
+        Random rand = new Random(); // Perdon, me gustan los randoms, no paint jajaja
+                                    // pero se imprimen las conexiones *no* dirigidas
+        int vertices = 0;
+        while (vertices < 6) {
+            int filaAleatoria = rand.nextInt(filas);
+            int columnaAleatoria = rand.nextInt(columnas);
+            if (matriz.get(filaAleatoria).get(columnaAleatoria) != 1) {
+                matriz.get(filaAleatoria).set(columnaAleatoria, 1);
+                matriz.get(columnaAleatoria).set(filaAleatoria, 1);
+                vertices++;
+            }
         }
 
         for (Vector<Integer> fila : matriz) { // por cada elemento en fila "x"
